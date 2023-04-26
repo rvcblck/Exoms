903d95d2e3e102e8dce54f6560aef3e34a5d3cd3
Justine Liwanag <justine.liwanag.t@bulsu.edu.ph>
2023-04-26 20:59:01 +0800

    attendance design update

--- frontend/src/app/user/attendance/attendance.component.ts
+++ frontend/src/app/user/attendance/attendance.component.ts
@@ -9,6 +9,12 @@ import { ProgramService } from 'src/app/program.service';
   styleUrls: ['./attendance.component.css']
 })
 export class AttendanceComponent implements OnInit {
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' });
+  }
+
   dates: string[] = [];
   attendanceIds: string[] = [];
   showAddDateForm = false;
